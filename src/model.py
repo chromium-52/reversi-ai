0063f60a5989044abfd584b878b8e21d56095fe1
typacchione <pacchione.t@northeastern.edu>
2022-11-04 19:44:14 -0400

    Model updates

--- src/model.py
+++ src/model.py
@@ -1,26 +1,37 @@
 from __future__ import annotations
 from typing import List, Tuple
 
+# A complete Reversi game state
 class State:
+    # The row and column dimension of the board for this game
     SIZE = 8
 
+    # Integer representations for each individual board cell state
     EMPTY = 0
     BLACK = 1
     WHITE = 2
 
-    # Default constructor that creates a board for a new game of Reversi
+    # Creates a new Reversi game state
+    # If board is None, creates a board for a new game
     def __init__(self, board: List[List[int]] = None) -> None:
         if board is None:
-            self.board = [[0 for _ in range(self.SIZE)] for _ in range(self.SIZE)]
+            self.initState()
+        else:
+            self.initState(board)
+    
+    # Initializes the board to the default Reversi starting board
+    def initState(self) -> None:
+        self.board = [[0 for _ in range(self.SIZE)] for _ in range(self.SIZE)]
 
-            self.board[3][3] = self.WHITE
-            self.board[3][4] = self.BLACK
-            self.board[4][3] = self.BLACK
-            self.board[4][4] = self.WHITE
+        self.board[3][3] = self.WHITE
+        self.board[3][4] = self.BLACK
+        self.board[4][3] = self.BLACK
+        self.board[4][4] = self.WHITE
 
-            self.numDisks = 4
-            return
+        self.numDisks = 4
 
+    # Initializes the board to any board state
+    def initState(self, board: List[List[int]]) -> None:
         self.board = board
 
         self.numDisks = 0
@@ -29,14 +40,14 @@ class State:
                 if cell != 0:
                     self.numDisks += 1
 
-    # 
+    # Returns the integer corresponding to whose turn it is
     def turn(self) -> int:
         return self.numDisks % 2 + 1
     
     # Places the disk on the board if the move is valid and returns the resulting state
     # If the move is not valid, the current state is returned
     def placeDisk(self, action: Action) -> State:
-        if self.validateMove():
+        if not self.validateMove():
             return self
 
         # function body
