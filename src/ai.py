a61a3c1261baf089b3976b1da1d20dde0a80e49f
Kenny Jung <dukyoung01@naver.com>
2022-11-04 19:05:45 -0400

    Add Minimax and Random

--- src/ai.py
+++ src/ai.py
@@ -1,5 +1,13 @@
 from src.model import GameState, Action
 
-class AI:
+class AI: # interface
     def find_action(self, game_state: GameState) -> Action:
         raise NotImplementedError("Method not implemented. Should be implemented in a subclass.")
+
+class Minimax(AI): # abstract class
+    def evaluation_function(self, game_state: GameState) -> int:
+        raise NotImplementedError("Method not implemented. Should be implemented in a subclass")
+
+class Random(Minimax):
+    def evaluation_function(self, game_state: GameState) -> int:
+        pass
---
+++ src/main.py
@@ -0,0 +1,5 @@
+def start_game() -> None:
+    pass
+
+if __name__ == '__main__':
+    start_game()
